cc4e630f17350b1f453dfef642a53739f0dbf157
AkhmadRezkyUtama261 <ahkmadrezkyutama369@gmail.com>
2026-06-16 12:03:30 +0700

    menambahkan datatable getprodi dan insert log

--- DAL.cs
+++ DAL.cs
@@ -187,6 +187,39 @@ namespace CRUDMahasiswaADO
             return dtMahasiswa;
         }
 
+        public void InsertLog(string message)
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            SqlCommand cmd = new SqlCommand("sp_LogMessage", conn);
+
+            cmd.Parameters.AddWithValue("@psn", message);
+
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.ExecuteNonQuery();
+        }
+
+        public DataTable getProdi()
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.Open();
+            }
+
+            SqlCommand cmd = new SqlCommand("select namaprodi from prodi", conn);
+            cmd.CommandType = CommandType.Text;
+
+            dtProdi = new DataTable();
+
+            da = new SqlDataAdapter(cmd);
+            da.Fill(dtProdi);
+
+            return dtProdi;
+        }
 
 
     }
